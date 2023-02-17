04d48a923f62cc506deaf87ee72bb92e0fc5d364
abdelghani-khalidi <51909290+abdelghani-khalidi@users.noreply.github.com>
2023-02-17 17:40:51 +0100

    Add files via upload

--- wms_carrier/__manifest__.py
+++ wms_carrier/__manifest__.py
@@ -1,36 +1,17 @@
-##############################################################################
-#
-#    Source code aggregation of :
-#    Copyright (C) 2016 OpenCrea SAS (<http://opencrea.fr>)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+
 {
 "name" : "Carrier module of the WMS",
     "category" : "Generic Modules",
-    "version" : "1.0",
-    "depends": ["crm", "calendar", "delivery", "sale"],
-    "author" : "OpenCrea",
+    "version" : "16.0",
+    "depends": ["delivery", "sale"],
+    "author" : "Abdelghani KHALIDI DK Group",
     "description": """
     carrier module for the Warehouse Management System:
     """,
-    # 'init_xml': [],
     "data": [
-    "view/carrier_view.xml",
-    "view/stock_picking_out_view.xml",
     "security/ir.model.access.csv",
+    "view/carrier_view.xml",
+    "view/stock_picking_view.xml",
     ],
     'demo_xml': [],
     'installable': True,
---
+++ wms_carrier/models/delivery_carrier_order.py
@@ -0,0 +1,147 @@
+
+from odoo import models
+from odoo import fields ,api
+from odoo import _
+import openerp.addons.decimal_precision as dp
+from odoo import SUPERUSER_ID
+from odoo import netsvc
+import unicodedata
+import time
+import datetime
+from dateutil.relativedelta import relativedelta
+import pytz
+import base64
+import os
+
+class delivery_carrier_order(models.Model):
+
+    _name = "delivery.carrier.order"
+    _description = "Carrier Order"
+    _inherit = ['mail.thread']
+
+    def _update_info(self):
+        res = {}
+        date_now = time.strftime('%Y-%m-%d %H:%M:%S')
+        for carrier_order in self:
+          if carrier_order:
+            order_date = carrier_order.date_expected or date_now
+            nagel_name = 'DZV_HENNART____CALTMS_IMP_AUFTRAEGE_'
+            nagel_counter = str(order_date).replace('-', '')
+            carrier_order.number_of_packages= 0
+            carrier_order.nb_picking= 0
+            carrier_order.nb_line=0
+            carrier_order.nb_container=0
+            carrier_order.nb_pallet= 0
+            carrier_order.nb_pallet_europe= 0
+            carrier_order.nb_pallet_perdu= 0
+            carrier_order.weight= 0
+            date = carrier_order.date_expected or date_now
+            carrier_order.nb_picking = len(carrier_order.picking_ids)
+            if carrier_order.picking_ids: 
+             for picking in carrier_order.picking_ids:
+                carrier_order.nb_line += len(picking.move_line_ids)
+                carrier_order.number_of_packages += picking.number_of_packages
+                carrier_order.nb_container += picking.nb_container
+                carrier_order.nb_pallet += picking.nb_pallet
+                carrier_order.nb_pallet_europe += picking.nb_pallet_europe
+                carrier_order.nb_pallet_perdu += picking.nb_pallet_perdu
+                date_delivered = picking.scheduled_date + datetime.timedelta(days=1)
+                carrier_order.date_delivered = date_delivered.strftime('%Y-%m-%d 12:00:00')
+                for line in picking.move_ids:
+                    carrier_order.weight += line.weight
+
+    def check_state(self):
+        for order in self:
+            test = order.state
+            if order.state not in ['cancel', 'done']:
+                states = []
+                for picking in order.picking_ids:
+                    if picking.state not in states:
+                        states.append(picking.state)
+                if "draft" in states:
+                    test = 'confirmed'
+                elif 'confirmed' in states:
+                    test = 'confirmed'
+                elif 'assigned' in states:
+                    test = 'assigned'
+                elif 'done' in states:
+                    test = 'done'
+                elif 'cancel' in states:
+                    test = 'cancel'
+            if order.state != test:
+               order.write({'state': test})
+        return order
+
+    
+    def button_action_done(self):
+        context = {}
+        location_obj = self.env['stock.location']
+        move_obj = self.env['stock.move']
+        picking_obj = self.env['stock.picking']
+        location_ids = location_obj.search([('usage', '=', 'customer')])
+        customer_location_id = location_ids[0]
+        move_output_ids = []
+        picking_ids = []
+        for order in self:
+            for picking in order.picking_ids:
+                picking_ids.append(picking.id)
+                for move in picking.move_line_ids:
+                    if move.state == 'done':
+                        move_output_ids.append(move.id)
+        date_done = picking.scheduled_date
+        moves = move_obj.search([('id','in',move_output_ids)])
+        for mm in moves:
+         mm.write({
+            'location_dest_id': customer_location_id,
+            'date': date_done,
+          
+            })
+
+        for order in self:
+            for picking in order.picking_ids:
+                if picking.state in ['assigned', 'confirmed','waiting']:
+                    picking.button_validate()
+        self.check_state()
+        return True
+
+    @api.depends('date_expected')
+    def compute_date_exep(self):
+        for rec in self:
+            if  rec.date_expected :
+                rec.date_expected2 = rec.date_expected.date()
+            else:
+                rec.date_expected2 = False
+
+    edi_done = fields.Boolean('EDI Already sending')
+    carrier_id = fields.Many2one("delivery.carrier", "Carrier")
+    date_done = fields.Datetime('Date of Transfer', index=True, help="Date of Completion")
+    date_expected = fields.Datetime('Date expected', index=True, help="Date", widget="date", readonly=True)
+    date_delivered = fields.Date(compute="_update_info", method=True )
+    date_expected2 = fields.Date(compute="compute_date_exep", store=True )
+    hour_expected = fields.Float('Hour expected', widget="float_time")
+    name = fields.Char('description')
+    warehouse_id = fields.Many2one('stock.warehouse', 'Warehouse', required=True, states={'done': [('readonly', True)]})
+    picking_ids = fields.One2many('stock.picking', 'carrier_order_id', 'Delivery Order')
+    state = fields.Selection([
+                        ('cancel', 'Cancelled'),
+                        ('draft', 'Draft'),
+                        ('confirmed', 'Waiting'),
+                        ('assigned', 'Available'),
+                        ('done', 'Done'),
+                        ], 'Status', index=True)
+
+  
+    weight = fields.Float(compute="_update_info", string='Weight', digits=dp.get_precision('Stock Weight'))
+    nb_line = fields.Integer(compute="_update_info", string='Nb line',)
+    nb_picking = fields.Integer(compute="_update_info", string='Nb picking',)
+    number_of_packages = fields.Integer(compute="_update_info", string='Nb packages')
+    nb_container = fields.Integer(compute="_update_info", string='Nb container',)
+    nb_pallet = fields.Integer(compute="_update_info", string='Nb pallet',)
+    nb_pallet_europe = fields.Integer(compute="_update_info", string='Nb Pallets europes')
+    nb_pallet_perdu = fields.Integer(compute="_update_info", string='Nb Pallets Perdus')
+    nb_pallet_ground = fields.Integer('Nb Pallets on Ground')
+
+    driver_name = fields.Char('Driver name')
+    temperature = fields.Float('Temperature')
+    note = fields.Text('Comment')
+
