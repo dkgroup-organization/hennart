0059360a3b5369e00467072474fe8e182f2d57e0
joannes59 <joannes.landy@dkgroup.fr>
2023-03-16 15:08:59 +0100

    scanner UI

--- wms_scanner/static/src/js/script.js
+++ wms_scanner/static/src/js/script.js
@@ -9,3 +9,28 @@ menuToggle.addEventListener("click", function () {
   main.classList.toggle("z-[-1]");
   main.classList.toggle("brightness-50");
 });
+
+window.addEventListener('DOMContentLoaded', function() {
+  document.querySelector("form input:first-child").focus()
+});
+
+// const emplacement = document.querySelector("#emplacement");
+
+// emplacement.addEventListener("click", function () {
+//   this.parentElement.classList.toggle("active");
+// });
+
+// document.addEventListener("click", function (event) {
+//   if (!emplacement.contains(event.target)) {
+//     emplacement.parentElement.classList.remove("active");
+//   }
+// });
+
+// const buttons = document.querySelectorAll("#select-1 button");
+// buttons.forEach(function (button) {
+//   button.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     const text = this.textContent.trim();
+//     emplacement.setAttribute("value", text);
+//   });
+// });
