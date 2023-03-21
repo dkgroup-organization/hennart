81f664e6e0d9b46f46d08c492cfecf7907263f42
Mehdi HJ <itsxenon@outlook.fr>
2023-03-21 11:52:33 +0100

    [ADD]: adapt account_move to synchronise account_invoice into it

---
+++ invoice_to_move/models/account_move.py
@@ -0,0 +1,25 @@
+
+
+from odoo import _, api, fields, models
+from odoo.tools import float_compare
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    picking_id = fields.Char(string="Bon de livraison")
+    incoterm_port = fields.Char(string="Port of entry")
+    incoterm_date = fields.Date(string="Date of arrival in UK")
+    payment_method_id = fields.Char(string="Methode de paiement")
+
+    total_ht = fields.Float(string='Total HT')
+    total_tva = fields.Float(string='Total TVA')
+    total_ttc = fields.Float(string='Total TTC')
+    piece_comptable = fields.Char(string='ID piece comptable')
+
+    account_id = fields.Many2one(
+        'account.account',
+        string='Account',
+        )
+
+    
