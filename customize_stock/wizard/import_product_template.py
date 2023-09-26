06dad330de0bb4004c33b7d1258dfcbdbea0e11f
Mehdi HJ <itsxenon@outlook.fr>
2023-09-26 10:32:29 +0100

    Update import_product_template.py

--- customize_stock/wizard/import_product_template.py
+++ customize_stock/wizard/import_product_template.py
@@ -41,68 +41,72 @@ class ImportPriceList(models.TransientModel):
         self.ensure_one()
         if not self.file:
             raise UserError(_("Please select a file to import."))
-        if not self.pricelist_id:
-            raise UserError(_("Please select a price list to import."))
 
-        product_ids = self.env['product.product'].search([('company_id', '=', False)])
-        product_ids.write({'company_id': self.env.user.company_id.id})
-
+        # Charger le fichier Excel depuis le champ binaire
         book = xlrd.open_workbook(file_contents=base64.b64decode(self.file))
         sheet = book.sheet_by_index(0)
         header = {}
+
         for i_col in range(sheet.ncols):
             header_name = sheet.cell_value(0, i_col)
             header[header_name] = i_col
 
+        # Parcourir les lignes du fichier Excel
         for row in range(1, sheet.nrows):
+            product_code = sheet.cell_value(row, header.get(self.column_mapping['default_code']))
+            if not product_code:
+                continue  # Ignorer les lignes sans default_code
 
-            product_code = sheet.cell_value(row, header.get('product_code'))
-            list_price = sheet.cell_value(row, header.get('list_price'))
+            # Rechercher le produit par default_code
+            product = self.env['product.template'].search([('default_code', '=', product_code)])
 
-            if product_code and list_price:
-                product = self.env['product.product'].search([('default_code', '=', product_code)])
-                if not product:
-                    raise UserError(_("This product code is unknown: {}".format(product_code)))
+            # Créer ou mettre à jour le produit en fonction de son existence
+            if not product:
+                product = self.env['product.template'].create({
+                    'default_code': product_code,
+                    'name': sheet.cell_value(row, header.get(self.column_mapping['name'])),
+                })
 
-                condition = [('product_id', '=', product.id), ('pricelist_id', '=', self.pricelist_id.id)]
-                item_ids = self.pricelist_id.item_ids.search(condition)
+            # Mapper les autres champs du fichier Excel aux champs Odoo ici pour mettre à jour le produit
+            product.write({
+                'name': sheet.cell_value(row, header.get(self.column_mapping['name'])),
+                'agrement_number': sheet.cell_value(row, header.get(self.column_mapping['agrement_number'])),
+                'region': sheet.cell_value(row, header.get(self.column_mapping['region'])),
+                # Allergen (Many2many)
+                # AOP
+                # Type fermier
+                'type_milk': sheet.cell_value(row, header.get(self.column_mapping['type_milk'])),
+                'heat_treatment_milk': sheet.cell_value(row, header.get(self.column_mapping['heat_treatment_milk'])),
+                'rennet': sheet.cell_value(row, header.get(self.column_mapping['rennet'])),
+                'salting': sheet.cell_value(row, header.get(self.column_mapping['salting'])),
+                # Allergen (Many2many)
+                # OGM
+                'nv_energy_kj': sheet.cell_value(row, header.get(self.column_mapping['nv_energy_kj'])),
+                'nv_energy_kc': sheet.cell_value(row, header.get(self.column_mapping['nv_energy_kc'])),
+                'nv_fat': sheet.cell_value(row, header.get(self.column_mapping['nv_fat'])),
+                'fat_in_dry_matter': sheet.cell_value(row, header.get(self.column_mapping['fat_in_dry_matter'])),
+                'nv_saturated_fatty_acids': sheet.cell_value(row, header.get(self.column_mapping['nv_saturated_fatty_acids'])),
+                'nv_carbohydrates': sheet.cell_value(row, header.get(self.column_mapping['nv_carbohydrates'])),
+                'nv_sugars': sheet.cell_value(row, header.get(self.column_mapping['nv_sugars'])),
+                'nv_protein': sheet.cell_value(row, header.get(self.column_mapping['nv_protein'])),
+                'nv_salt': sheet.cell_value(row, header.get(self.column_mapping['nv_salt'])),
+            })
+            
+            # Récupérer les valeurs de la colonne ingredient et les diviser par ","
+            ingredient_values = sheet.cell_value(row, header.get(self.column_mapping['ingredient'])).split(',')
+            
+            # Nettoyer et créer les enregistrements product.ingredient si nécessaire
+            ingredient_ids = []
+            for ingredient_value in ingredient_values:
+                ingredient_name = ingredient_value.strip()
+                if ingredient_name:
+                    # Rechercher ou créer un enregistrement product.ingredient avec le nom
+                    ingredient = self.env['product.ingredient'].search([('name', '=', ingredient_name)], limit=1)
+                    if not ingredient:
+                        ingredient = self.env['product.ingredient'].create({'name': ingredient_name})
+                    ingredient_ids.append(ingredient.id)
+            
+            # Attacher les enregistrements product.ingredient au produit
+            product.write({'ingredient_ids': [(6, 0, ingredient_ids)]})
 
-                item_values = {
-                        'pricelist_id': self.pricelist_id.id,
-                        'product_id': product.id,
-                        'fixed_price': list_price,
-                        'compute_price': 'fixed',
-                        'applied_on': '0_product_variant',
-                    }
-
-                if self.pricelist_id.id == 1:
-                    product.list_price = list_price
-                elif item_ids:
-                    item_ids.write(item_values)
-                else:
-                    item_ids.create(item_values)
         return True
-
-    def from_data(self):
-        fields = ['product_code', 'product_name', 'list_price']
-
-        rows = []
-
-        if self.pricelist_id:
-            for item in self.pricelist_id.item_ids:
-                if item.compute_price == "fixed" and item.product_id.default_code:
-                    data = [str(item.product_id.default_code), str(item.product_id.name), float(item.fixed_price)]
-                    rows.append(data)
-        else:
-            product_prices = self.env['product.product'].search([('default_code', '!=', False)])
-            for product in product_prices:
-                data = [str(product.default_code), str(product.name), float(product.list_price)]
-                rows.append(data)
-
-        with ExportXlsxWriter(fields, len(rows)) as xlsx_writer:
-            for row_index, row in enumerate(rows):
-                for cell_index, cell_value in enumerate(row):
-                    xlsx_writer.write_cell(row_index + 1, cell_index, cell_value)
-
-        return xlsx_writer.value
-
