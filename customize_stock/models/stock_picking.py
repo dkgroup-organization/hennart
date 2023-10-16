1bd5016d56f64e0820b9a4b3fa92a5c837f1aab6
Mehdi HJ <12463833+mehdihj@users.noreply.github.com>
2023-10-16 17:55:24 +0100

    update

--- customize_stock/models/stock_picking.py
+++ customize_stock/models/stock_picking.py
@@ -70,7 +70,8 @@ class StockPicking(models.Model):
 
     def action_mrp(self):
         for picking in self:
-            for move_line in picking.move_lines:
+            for move_line in picking.move_line_ids:
+                # if move_line.state == 'assigned' and move_line.product_uom_qty > 0:
                 if move_line.state == 'assigned' and move_line.product_uom_qty > 0:
 
                     production_order = self.env['mrp.production'].create({
