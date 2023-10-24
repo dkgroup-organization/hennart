a3294a63106e70b64f13f8b418e91c2c26b29539
Mehdi HJ <12463833+mehdihj@users.noreply.github.com>
2023-10-24 12:32:43 +0100

    Update stock_picking.py

--- customize_stock/models/stock_picking.py
+++ customize_stock/models/stock_picking.py
@@ -98,7 +98,6 @@ class StockPicking(models.Model):
                                 in_process_productions.write({
                                     'product_qty': quantity
                                     })
-                                raise UserError('TEST 1')
                                 
                             else:
                                 production_order = self.env['mrp.production'].create({
@@ -107,7 +106,6 @@ class StockPicking(models.Model):
                                     'move_from_picking_ids': [(4, move.id)],  
 
                                 })
-                                raise UserError('TEST 2')
 
                             # production_order.action_confirm()  # Confirmer le MO
                             # production_order.button_plan()  # Planifier le MO
