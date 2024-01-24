215ff92a6711364cbc9cd94f3704dfb4258cfc92
joannes59 <joannes.landy@dkgroup.fr>
2024-01-24 18:20:30 +0100

    save sscc label

--- wms_sscc/models/stock_picking.py
+++ wms_sscc/models/stock_picking.py
@@ -7,46 +7,33 @@ class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     sscc_lines_ids = fields.One2many('stock.sscc', 'picking_id', 'Code colis SSCC')
-    nb_sscc_label = fields.Integer('Needed sscc label:')
+    nb_total_sscc = fields.Integer('Total label', compute='compute_total_sscc')
 
-    @api.model
-    def create(self, vals):
-        obj = super().create(vals)
-        if obj:
-            obj.update_sscc()
-        return obj
+    @api.depends('sscc_lines_ids')
+    def compute_total_sscc(self):
+        """ return total label """
+        for picking in self:
+            picking.nb_total_sscc = len(picking.sscc_lines_ids)
 
-    def write(self,values):
-        override_write = super().write(values)
-        self.update_sscc()
-        return override_write
-
-    # @api.depends('nb_sscc_label')
+    @api.depends('nb_container', 'nb_pallet')
     def update_sscc(self):
         for picking in self:
-            # nb_total_container = picking.nb_container + picking.nb_pallet
-            nb_total_container = picking.nb_sscc_label
-            sscc_lines_ids = []
-            for sscc in picking.sscc_lines_ids:
-                sscc_lines_ids.append(sscc.id)
+            if picking.picking_type_code != 'outgoing':
+                continue
+            nb_total_container = picking.nb_container + picking.nb_pallet
+            sscc_lines_ids = picking.sscc_lines_ids.mapped('id')
             todo_sscc = nb_total_container - len(sscc_lines_ids)
             if todo_sscc == 0:
                 pass
             elif todo_sscc > 0:
-                #create sscc
-                for i in range(todo_sscc):
-
-                    new_idd = self.env['stock.sscc'].create({'picking_id':picking.id})
-                    new_idd.picking_id = picking.id
-                    sscc_lines_ids.append(new_idd.id)
-            else:
-         
-                for i in range(-todo_sscc):
-                    test = self.env['stock.sscc'].search([('picking_id','=',picking.id)],order="id desc", limit=1)
+                # Create SSCC
+                while todo_sscc:
+                    self.env['stock.sscc'].create({'picking_id': picking._origin.id})
+                    todo_sscc -= 1
+            elif todo_sscc < 0:
+                # Delete SSCC
+                while todo_sscc:
+                    test = self.env['stock.sscc'].search([('picking_id', '=', picking._origin.id)],
+                                                         order="id desc", limit=1)
                     test.unlink()
-
- 
-
-
-
-
+                    todo_sscc += 1
--- wms_sscc/models/stock_sscc.py
+++ wms_sscc/models/stock_sscc.py
@@ -23,25 +23,38 @@ class stock_sscc(models.Model):
     _description = "SSCC Label"
     _order = "name"
 
-    @api.depends('serial', 'cnuf', 'identifier')
-    def _update_info(self):
-
-        for label in self:
-            sscc = "%s%s%s" % (label.prefixe, label.cnuf, label.serial)
-            label.name = "%s%s%s" % (label.identifier, sscc, ean_checksum(sscc))
-
-    number = fields.Integer('Colis Numero:')
-    picking_id = fields.Many2one('stock.picking', string='Picking')
-    name = fields.Char(compute="_update_info", string='SSCC', store=True, index=True)
+    picking_id = fields.Many2one('stock.picking', string='Picking', index=True)
+    nb_sscc = fields.Integer('nb_sscc', compute='compute_nb_sscc', store=False)
+    nb_total_sscc = fields.Integer(related='picking_id.nb_total_sscc')
+    name = fields.Char(compute="compute_name", string='SSCC', store=True, index=True)
     identifier = fields.Char('IDENTIFIER', default='00')
     prefixe = fields.Char('PREFIXE', default='0')
     cnuf = fields.Char('CNUF', default='7002221')
     serial = fields.Char('SERIAL', default='/')
 
+    def compute_nb_sscc(self):
+        """ get numerotation of sscc """
+        for label in self:
+            label_ids = self.search([('picking_id', '=', label.picking_id.id)])
+            number = 1
+            for label_check in label_ids:
+                if label_check == label:
+                    break
+                else:
+                    number += 1
+            label.nb_sscc = number
+
+    @api.depends('serial', 'cnuf', 'identifier')
+    def compute_name(self):
+        """ Compute the number of the SSCC """
+        for label in self:
+            sscc = "%s%s%s" % (label.prefixe, label.cnuf, label.serial)
+            label.name = "%s%s%s" % (label.identifier, sscc, ean_checksum(sscc))
+
     @api.model
     def create(self, vals):
         obj = super(stock_sscc, self).create(vals)
         if obj.serial == '/':
-            number = self.env['ir.sequence'].get('stock.sscc.code') or '/'
-            obj.write({'serial': number})
+            serial = self.env['ir.sequence'].get('stock.sscc.code') or '/'
+            obj.write({'serial': serial})
         return obj
