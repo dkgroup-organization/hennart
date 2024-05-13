0a02f2936b63e2b76886b8b52749dd0f173842ca
Mehdi HJ <12463833+mehdihj@users.noreply.github.com>
2024-05-13 11:08:11 +0100

    update

---
+++ report_invoice_uk/__manifest__.py
@@ -0,0 +1,16 @@
+
+
+{
+    "name": "Hennart Report invoice UK",
+    "version": "16.0.1.1.1",
+    "category": "Misc",
+    "license": "AGPL-3",
+    "author": "Mehdi HAJJI ",
+    "website": "https://dkgroup.fr/",
+    "depends": ["web", "sale", "account", "stock", "product", "customize_product_template"],
+    "data": [
+        "views/report_invoice_uk_view.xml",
+        "reports/account_report.xml",
+        "reports/report_invoice.xml",
+    ],
+}
