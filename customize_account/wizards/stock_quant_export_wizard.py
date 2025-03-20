e02609ed2c1c7c8b827f036c7d5bcc46ca68c386
joannes59 <joannes.landy@dkgroup.fr>
2025-03-20 09:50:00 +0100

    export stock

--- customize_account/wizards/stock_quant_export_wizard.py
+++ customize_account/wizards/stock_quant_export_wizard.py
@@ -1,4 +1,8 @@
 from odoo import models, fields, api
+import base64
+import io
+import xlsxwriter
+
 
 class StockQuantExportWizard(models.TransientModel):
     _name = 'stock.quant.export.wizard'
@@ -45,13 +49,61 @@ class StockQuantExportWizard(models.TransientModel):
         self.env.cr.execute(sql)
         result_sql = self.env.cr.fetchall()
 
-        data = []
+        output = io.BytesIO()
+        workbook = xlsxwriter.Workbook(output)
+        worksheet = workbook.add_worksheet(self.date.strftime('stock_%Y_%m_%d'))
+        # Création du titre
+        worksheet.write(0, 0, 'code')
+        worksheet.write(0, 1, 'produit')
+        worksheet.write(0, 2, 'lot')
+        worksheet.write(0, 3, 'Quantité')
+
+        row = 1
         for line in result_sql:
             product = self.env['product.product'].browse(line[0])
             lot = self.env['stock.lot'].browse(line[1])
             quantity = float(line[2])
-            print(product.default_code, product.name, lot.ref, quantity)
+
+            worksheet.write(row, 0, product.default_code)
+            worksheet.write(row, 1, product.name)
+            worksheet.write(row, 2, lot.ref)
+            worksheet.write(row, 3, quantity)
+            row += 1
+
+        # Finaliser et enregistrer le fichier
+        workbook.close()
+        output.seek(0)
+        file_data = base64.b64encode(output.read()).decode('utf-8')
+        file_name = self.date.strftime('stock_%Y_%m_%d.xl.xlsx')
+
+        attachment = self.env['ir.attachment'].create({
+            'name': file_name,
+            'datas': file_data,
+            'type': 'binary',
+            'mimetype': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        })
+
+        return {
+            'type': 'ir.actions.act_url',
+            'url': f'/web/content/{attachment.id}?download=true',
+            'target': 'self',
+        }
 
 
+    def action_download_file(self):
+        output = b"Hello, this is your file content!"  # Remplace par ton fichier
+        file_data = base64.b64encode(output).decode('utf-8')
+        file_name = "mon_fichier.txt"
 
-        # return self.env['stock.quant.export'].create({}).export_stock_quant()
+        attachment = self.env['ir.attachment'].create({
+            'name': file_name,
+            'datas': file_data,
+            'type': 'binary',
+            'mimetype': 'text/plain'
+        })
+
+        return {
+            'type': 'ir.actions.act_url',
+            'url': f'/web/content/{attachment.id}?download=true',
+            'target': 'self',
+        }
